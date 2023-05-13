1bb0cad7832890c883a734a0bb9d5471307505b0
Juan <jpichardo369@gmail.com>
2023-05-13 04:02:40 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  let infoDump = [
+    "I prefer to sleep more than stay awake",
+    "my favorite anime is JoJo's Bizarre Adventure",
+    "I can wiggle my ears",
+    "I have ADHD",
+    "my favorite cartoon is SpongeBob SquarePants & The Simpsons",
+    "I've been programming since 2018",
+    "I hate it when people repeat themselves",
+    "my favorite number is 22",
+    "I love candles",
+    "my favorite hobbies outside of computers are drawing & listening to music",
+    "my birthday is on February 22",
+    "I have 3 dogs, two German shepherds & one pitbull"
+  ]
+  let info = infoDump[Math.floor((Math.random()*infoDump.length))]
+</script>
+
+<div class="container flex justify-items-center mx-auto flex-col p-8 gap-4">
+  <h1 class="text-2xl text-slate-200 text-center font-bold pt-6"><strong class="text-sky-300">minota</strong>.club <h2 class="text-lg text-slate-500">he/him</h2></h1>
+  <h2 class="text-xl text-slate-300 text-center font-bold pt-4">About me</h2>
+  <p class="text-lg text-slate-200 text-center ml-96 mr-96 -mt-2">
+    Hello, my name is Juan Pichardo, I hail from New York. My nationality is Dominican & Honduran.
+    I currently attend high school & I intend to study Software Engineering in college.
+    There's not really anything interesting to say about myself, however, {info}.
+  </p>
+  <h2 class="text-xl text-slate-300 text-center font-bold pt-4">Reach me</h2>
+  <h2 class="text-lg text-slate-300 text-center -mt-2 pb-6"><a class="hover:text-sky-400" href="https://github.com/minotaa">GitHub</a> | <a class="hover:text-sky-400" href="https://twitter.com/minotadev">Twitter</a> | <a class="hover:text-sky-400" href="https://ko-fi.com/minota">Ko-fi</a> | <a class="hover:text-sky-400" href="https://namemc.com/ec3b5621-7313-4b7e-a8e3-91b3d63c4e24">Minecraft</a> | <a class="hover:text-sky-400" href="https://twitch.tv/xMinota">Twitch</a></h2>
+</div>
