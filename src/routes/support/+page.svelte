89f6ef7496ee18fac4449ac49f17018282d71627
minotaa <jpichardo369@gmail.com>
2024-08-08 15:49:20 -0400

    yewsa

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,4 @@
+<div class="pl-4 md:mx-auto flex justify-items-center mx-auto flex-col pt-4">
+  <p class="text-xl text-white">Contact me on <a class="text-sky-500" href="https://discord.gg/sd23CeBZBe">this Discord</a> or by <a class="text-sky-500" href="mailto:me@minota.cc">email</a>.</p>
+  <p class="text-lg text-white">We do not collect any data except cursory data that Apple provides to us by default.</p>
+</div>
