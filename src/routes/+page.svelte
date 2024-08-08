951a763a90d1226435399d77f196fd9362bc9d34
minotaa <jpichardo369@gmail.com>
2024-08-08 15:31:38 -0400

    YEAH

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,13 @@
 <script>
-  let infoDump = [
-    "I prefer to sleep more than stay awake",
-    "my favorite anime is JoJo's Bizarre Adventure",
-    "I can wiggle my ears",
-    "I have ADHD",
-    "my favorite cartoon is SpongeBob SquarePants & The Simpsons",
-    "I've been programming since 2018",
-    "I hate it when people repeat themselves",
-    "my favorite number is 22",
-    "I love candles",
-    "my favorite hobbies outside of computers are drawing & listening to music",
-    "my birthday is on February 22",
-    "I have 3 dogs, two German shepherds & one pitbull",
-    "my eyes are brown",
-    "I can speak Spanish",
-    "I can't breathe"
-  ]
-  let info = infoDump[Math.floor((Math.random()*infoDump.length))]
+  //let info = infoDump[Math.floor((Math.random()*infoDump.length))]
 </script>
 
-<div class="pt-28 flex justify-center items-center">
-  <img src="/photos/locked.webp" height="600" width="600"/>
+<div class="md:container md:mx-auto flex justify-items-center mx-auto flex-col p-8 gap-4">
+  <h1 class="text-2xl text-slate-200 text-center font-bold pt-6"><strong class="text-sky-300">minota</strong>.cc</h1>
+  <h2 class="text-lg text-center text-slate-500 -mt-4 -mb-6">he/him</h2>
+  <p class="text-lg place-self-center sm:max-w-lg text-slate-200 text-center mt-4">
+    I like playing, streaming and making games :-)
+  </p>
+  <h2 class="text-xl text-slate-300 text-center font-bold pt-4">Reach me</h2>
+  <h2 class="text-lg text-slate-300 text-center -mt-2 pb-6">Discord: <code>minota</code> | <a class="hover:text-sky-400" href="https://github.com/minotaa">GitHub</a> | <a class="hover:text-sky-400" href="https://twitter.com/minota_cc">Twitter</a> | <a class="hover:text-sky-400" href="mailto:me@minota.cc">Email</a></h2>
 </div>
