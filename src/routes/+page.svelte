56e5f1329bf78885eb993d95c28fb23f1993da65
minota <34592028+minotaa@users.noreply.github.com>
2025-04-04 03:14:12 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
   }
 </script>
 
-<div class="md:container md:mx-auto flex mx-auto flex-col pt-8 gap-4 mb-4">
+<div class="md:container md:mx-auto flex mx-auto flex-col pl-4 pt-8 gap-4 mb-4">
   <div class="flex md:flex-row flex-col">
     <h1 class="text-2xl text-slate-200 font-bold text-center md:text-left"><strong class="text-sky-300">minota</strong></h1>
     <h2 class="text-lg text-slate-500 md:ml-3 mt-1 text-center md:text-left">he/him</h2>
@@ -56,7 +56,7 @@
         Internally known as Rhode Island. It is my first available Godot game. It's a small fishing game where you fish for fish to get more rods, baits, etc. It's still available on the App Store right now!
       </h3>
       <div class="flex flex-col w-fit place-end">
-        <a title="Get Beachside Tactics" href="https://apps.apple.com/us/app/beachside-tactics/id6596775495?itscg=30200&itsct=apps_box_badge&mttnsubad=6596775495" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 mt-3 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
+        <a title="Get Beachside Tactics" href="https://apps.apple.com/us/app/beachside-tactics/id6596775495?itscg=30200&itsct=apps_box_badge&mttnsubad=6596775495" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
       </div>
     </div>
 
@@ -66,7 +66,7 @@
         Murkwood? Myrkwood is a game inspired by an old Flash game, Three Goblets, in which you progress through the game by automatically fighting enemies and crafting items from its drops. Now on the App Store.
       </h3>
       <div class="flex flex-col w-fit place-end">
-        <a title="Get Myrkwood" href="https://apps.apple.com/us/app/myrkwood/id6738539616" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 mt-3 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
+        <a title="Get Myrkwood" href="https://apps.apple.com/us/app/myrkwood/id6738539616" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
       </div>
     </div>
 
@@ -77,7 +77,7 @@
         Bulletlane is the third and newest game I made, it's a car based shoot 'em up in which you defeat enemies, get powerups, and upgrade your weapons, cars, and other stuff. It was inspired by an old game I played on the App Store, now I made a new version of it. Now on the App Store.
       </h3>
       <div class="flex flex-col w-fit place-end">
-        <a title="Get Bulletlane" href="https://apps.apple.com/us/app/bulletlane/id6743356289" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 mt-3 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
+        <a title="Get Bulletlane" href="https://apps.apple.com/us/app/bulletlane/id6743356289" target="_blank" class="shadow-lg rounded-xl pt-2 pb-2 pl-4 pr-4 bg-sky-400 hover:bg-sky-500 transition-colors">App Store</a>
       </div>
     </div>
 
